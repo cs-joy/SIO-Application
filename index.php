4168e7b2d889005498f87e6d1f64861d27132480
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-29 21:56:19 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home | SIO Application</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container my-5">
+        <div class="h4 pb-2 mb-4 text-primary border-bottom border-primary">
+            <h3>SIO Dashboard</h3>
+        </div>
+        <div class="p-3 bg-info bg-opacity-10 border border-info border-start-0 rounded-end">
+            <h4>Welcome to SIO Profile</h4>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+  </body>
+</html>
