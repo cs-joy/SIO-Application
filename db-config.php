c792789a74fa015826dd1e473b8d8efb56171ae9
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-29 20:22:33 +0600

    rmv

--- db-config.php
+++ db-config.php
@@ -1,3 +1,15 @@
 <?php
 
+$server = getenv('SERVER');
+$username = getenv('USERNAME');
+$database = getenv('DATABASE');
+$password = getenv('PASSWORD');
 
+try {
+    $db_connection = new PDO("mysql:host=$server;dbname=$database", $username, $password);
+    $db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connection Successfully!";
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
