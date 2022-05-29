2d7b9a665d80a0d2f3f1d50b4c3d89681457261a
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-29 21:44:03 +0600

    database connection configuration added
    with composer *packagist

--- db-config.php
+++ db-config.php
@@ -1,15 +1,27 @@
 <?php
+require __DIR__ . '/vendor/autoload.php';
 
-$server = getenv('SERVER');
-$username = getenv('USERNAME');
-$database = getenv('DATABASE');
-$password = getenv('PASSWORD');
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$server = $_ENV['SERVER'];
+$username = $_ENV['USERNAME'];
+$database = $_ENV['DATABASE'];
+$password = $_ENV['PASSWORD'];
 
 try {
     $db_connection = new PDO("mysql:host=$server;dbname=$database", $username, $password);
     $db_connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    echo "Connection Successfully!";
+    echo "Database Connection Successfull!";
 } catch(PDOException $e) {
     echo "Connection failed: " . $e->getMessage();
 }
+
+/*
+$con = new mysqli($server, $username, $password, $database);
+if($con->connect_error){
+    echo "Connection failed: " . $con->connect_error;
+}
+echo "Connection Successfully!";
+*/
