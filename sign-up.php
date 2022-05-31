d1f29d689815bd9389c0b14f0a96f4b9d889e3b4
cryptosky-platform <58134242+cs-joy@users.noreply.github.com>
2022-05-31 20:05:27 +0600

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -125,6 +125,9 @@ if(isset($_POST['signup'])) {
         </form>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
+    <script type="text/javascript"></script>
 </body>
 
 </html>
